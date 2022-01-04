726fdba64eb276a4a87887be301a9271e3901b06
Chinchanchanchin <chanchin2102@gmail.com>
2022-01-04 23:21:17 +0900

    them

--- if/tank.js
+++ if/tank.js
@@ -1,28 +1,28 @@
 
-   
-    let boom = parseInt(document.getElementById("boom"));
+    let tank = document.getElementById("tank");
+    let boom = document.getElementById("boom");
     let top_boom = parseInt(boom.style["top"]);
     let left_boom = parseInt(boom.style["left"]);
 
     function len () {
-        let  top_tank = parseInt(document.getElementById("tank").style["top"]);
-        let  left_tank = parseInt(document.getElementById("tank").style["left"]);
+        let  top_tank = parseInt(tank.style["top"]);
+        let  left_tank = parseInt(tank.style["left"]);
         tank.style["top"] = top_tank - 19 + "px";
 
-        top_tank = document.tank.style["top"];
-        left_tank = tank.style["left"];
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
         if (top_tank == top_boom && left_tank == left_boom) {
-            document.getElementById("tank").style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')";
-            document.getElementById("tank").style.zIndex = 1;
+            tank.style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')";
+            tank.style.zIndex = 1;
         }
     }
     function xuong () {
         let  top_tank = parseInt(document.getElementById("tank").style["top"]);
         let  left_tank = parseInt(document.getElementById("tank").style["left"]);
-        document.getElementById("tank").style["top"] = top_tank + 19 + "px";
+        tank.style["top"] = top_tank + 19 + "px";
 
-        top_tank = document.tank.style["top"];
-        left_tank = tank.style["left"];
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
         if (top_tank == top_boom && left_tank == left_boom) {
             document.getElementById("tank").style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')";
             document.getElementById("tank").style.zIndex = 1;
@@ -31,10 +31,10 @@
     function trai () {
         let  top_tank = parseInt(document.getElementById("tank").style["top"]);
         let  left_tank = parseInt(document.getElementById("tank").style["left"]);
-        document.getElementById("tank").style["left"] = left_tank - 19 + "px";
+        tank.style["left"] = left_tank - 19 + "px";
 
-        top_tank = document.tank.style["top"];
-        left_tank = tank.style["left"];
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
         if (top_tank == top_boom && left_tank == left_boom) {
             document.getElementById("tank").style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')";
             tank.style.zIndex = 1;
@@ -45,8 +45,8 @@
         let  left_tank = parseInt(document.getElementById("tank").style["left"]);
         tank.style["left"] = left_tank + 19 + "px";
 
-        top_tank = document.tank.style["top"];
-        left_tank = tank.style["left"];
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
         if (top_tank == top_boom && left_tank == left_boom) {
             document.getElementById("tank").style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')";
             tank.style.zIndex = 1;
