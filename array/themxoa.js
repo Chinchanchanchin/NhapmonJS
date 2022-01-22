5becf3990ecaebe43440da6b330b93621ff3712f
Chinchanchanchin <chanchin2102@gmail.com>
2022-01-22 23:46:22 +0900

    them

---
+++ array/themxoa.js
@@ -0,0 +1,23 @@
+let arr = ["Nguyễn Văn A", "Nguyễn Văn B"];
+
+function Add() {
+    let name = document.getElementById('name').value;
+   
+    arr.push (name);
+    
+}
+
+function Delete() {
+    arr.pop();
+}
+
+
+function Display() {
+    let list = "";
+    for (let i = 0; i < arr.length; i++) {
+        list+= arr[i] + "</br>";
+        
+    }
+    document.getElementById("result").innerHTML = list;
+}
+
